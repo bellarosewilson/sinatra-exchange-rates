9f87be3376660da75f2b6a0a6780879cbc7aa6cf
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-11 22:26:14 +0000

    erb added

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
-require "sinatra"
-require "sinatra/reloader"
+require 'sinatra'
+require 'httparty'
+require 'nokogiri'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+BASE_URL = "https://api.exchangerate.host/latest"
+
+def fetch_currency_data
+  response = HTTParty.get(BASE_URL)
+  response.parsed_response["rates"] if response.code == 200
+end
+
+get '/' do
+  @rates = fetch_currency_data
+  halt 500, "Error fetching currency data" unless @rates
+  erb :index
+end
+
+get '/:currency' do
+  @currency = params[:currency].upcase
+  @rates = fetch_currency_data
+  halt 404, "Currency not found" unless @rates&.key?(@currency)
+  erb :currency
+end
+
+get '/:from/:to' do
+  @from = params[:from].upcase
+  @to = params[:to].upcase
+  @rates = fetch_currency_data
+  halt 404, "Currency not found" unless @rates&.key?(@from) && @rates.key?(@to)
+  @exchange_rate = (@rates[@to] / @rates[@from]).round(6)
+  erb :exchange
 end

0e9fdf376d20c641e24fc69120ad0e6f24701dcf
Bella Rose Wilson <bwilson9893@gmail.com>
2025-03-12 16:39:01 +0000

    step two save

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require "sinatra/reloader"
 require "http"
 
 get("/") do
-  @raw_response = HTTP.get("api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"")
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch{"("EXCHANGE_RATE_KEY")}")
 
   @string_response = @raw_response.to_s
 
@@ -14,9 +14,29 @@ get("/") do
   erb (:homepage)
 end
 
-get("/:first_symbol") do
+get("/:from_currency") do
   @the_symbol = params.fetch("first_symbol") 
+ 
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
 
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+ 
+  @currencies = @parsed_response.fetch ("currencies")
   erb(:step_one)
+
+get("/:from_currency/:to_currency") do
+  @from_currency = params.fetch("from_currency")
+  @to_currency = params.fetch("to_currency")
+
+  @raw_response = HTTP.get("https://api.exchangerate.host/convert?from=#{@first_symbol}&to=#{@second_symbol}&amount=1&access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}")
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @conversion = @parsed_response.fetch("result")
+  erb(:step_two)
 
 end
